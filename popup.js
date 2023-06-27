af504563639c769d131eade8adcf98c242101435
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-06-27 21:22:54 +0100

    added popup.js

---
+++ popup.js
@@ -0,0 +1,55 @@
+export const works = [
+  {
+    title: 'Multi Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+  },
+  {
+    title: 'Multi Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+  },
+  {
+    title: 'Multi Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+  },
+  {
+    title: 'Multi Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+  },
+  {
+    title: 'Multi Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+  },
+  {
+    title: 'Multi Post Stories',
+    subtitle: 'Gain+Glory',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+  },
+];
+
+export const detail = [
+  {
+    title: 'Keeping track of hundreds of people',
+    tags: [
+      'Codekit',
+      'GitHub',
+      'JavaScript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+      'Ruby on Rails',
+      'CSS',
+      'Javascript',
+    ],
+    paragraph: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.`,
+    desparagraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveLink: 'https://danielad10.github.io/My-Portfolio/',
+    sourceLink: 'https://github.com/DanielAD10/My-Portfolio',
+    image: './assets/images/detail-des-img.svg',
+  },
+];
